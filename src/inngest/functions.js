9741f49d28eaad06e906b5e13ac5592b7d05ec6d
Abdullah Ansari <120268836+trulyabdullah@users.noreply.github.com>
2026-09-25 20:11:29 +0530

    minor

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -273,7 +273,7 @@ export const codeAgentFunction = inngest.createFunction(
 		});
 
 		await step.run("save-result", async () => {
-			if (isError)
+			if (isError) {
 				return await db.message.create({
 					data: {
 						projectId: event.data.projectId,
@@ -282,6 +282,7 @@ export const codeAgentFunction = inngest.createFunction(
 						type: MessageType.ERROR,
 					},
 				});
+			}
 
 			return await db.message.create({
 				data: {
